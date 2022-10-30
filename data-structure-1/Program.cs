69e62ae7393cd8b12c57a2897306c75a167b8b3e
keremeersoy <azimetkeremersoy@gmail.com>
2022-10-30 13:24:32 +0300

    Add project files.

---
+++ data-structure-1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace data_structure_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] x = new int[10];
+            int[,] y = new int[2, 3]; //iki boyutlu dizi
+            int[,,] z = new int[2, 3, 4];//2 satır, 3'e bölünmüş, 4
+                                         //z[0,1,2]
+                                         //z[b1, b2, b3] --> new int[2,3,4]
+                                         //z[i1, i2, i3] --> z[1,1,1] -> b2*b3*i1, i2*b3, i3*4 ADRES
+            int[,,,] q = new int[4, 4, 2, 4]; //4 grup var, her bir dörtlü 4e ayrılmış, her bir dörtlü grup 2 ye bölünmüş. her bir ikili grup 4e bölünmüş.
+            int[,,,] kku = new int[8, 10, 2, 4];
+            //müh fak.     bilg müh nö.    1.sınıf    100
+            //müh fak.     bilg müh nö.    2.sınıf    120
+            // kku[b1, b2, b3, b4]
+            // kku[i1, i2, i3, i4] adresi nedir?
+            //toplam + (i1*b2*b3*b4 + i2*b3*b4 + i3*b4 + i4)*4
+            //en fazla 1.sınıf hangi bölümde?
+            //birinci sınıflarda en çok hangi fakültede öğrenci var?
+            kku[0, 0, 0, 1] = 120;
+            int toplam = 0;
+            int ebb = 0;
+            int fakulteindis = -1;
+
+            for (int fak = 0; fak < 8; fak++)
+            {
+                for (int n_i = 0; n_i < 10; n_i++)
+                {
+                    for (int sin = 0; sin < 2; sin++)
+                    {
+                        toplam += kku[fak, n_i, sin];
+                    }
+                    if (ebb < toplam)
+                    {
+                        ebb = toplam;
+                        fakulteindis = fak;
+                    }
+
+                }
+            }
+
+            //recursive yapı;
+            // #tek boyutlu halini ise finalde sorucam:)
+            // ikinci sınıfları 1.sınıflardan fazla olan
+            // en fazla öprencisi olan 2. fakülte
+
+
+
+            y[1, 0] = 8;
+
+            int toplam = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    toplam += y[j, i];
+                }
+            }
+
+            // y[i1, i2] = y[1,1] = Başlangıç adresi + i1* satır boyutu + i2*4
+            // her bir hücre 4byte
+            // x[0]  1000. byte dan başlıyor
+            // x[indis] = x[0] adres + indis*4
+            // x[5] = 5*4 + 1000;
+
+            x[9] = 9;
+            if (int i = 0; i < 0; i++)
+    {
+                x[i] = i;
+            }
+
+
+            string s = "";
+            s = "abc";
+            s = "123";
+            int a = 1;
+            int b = 0;
+            b = min(1, 2, 3);
+            if (a > b) a = b;
+            else b = a;
+            Console.WriteLine(a);
+
+            //değişkenlerin ram de saklandığı alanlar vardır
+            //değişkenler ram'de yan yanadır. arada boşluklar olmaz.
+            //string az kulllanıcaz çünkü performans düşmanı!!!!!!pointerdır
+
+        }
+        static int min {int a, int b, int c}
+
+}
+}
---
+++ veri-yapıları-1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace veri_yapıları_1
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node ( int data )
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    class List
+    {
+        Node head;
+
+        public List()
+        {
+            head = null;
+        }
+
+        public void sonaEkle ( int data )
+        {
+            Node eleman = new Node(data);
+
+            if ( head == null )
+            {
+                head = eleman;
+                Console.WriteLine("liste oluşturuldu, ilk düğüm eklendi");
+            }
+            else
+            {
+                Node temp = head;
+
+                while ( temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = eleman;
+                Console.WriteLine("sona eleman eklendi");
+            }
+        }
+
+        public void basaEkle ( int data )
+        {
+            Node eleman = new Node(data);
+
+            if( head == null )
+            {
+                head = eleman;
+                Console.WriteLine("liste oluşturuldu, ilk eleman eklendi");
+            }
+            else
+            {
+                eleman.next = head; // listenin başına eleman eklendi
+                head = eleman;   // eklenen düğümümn ismi head oldu
+
+                Console.WriteLine("başa eleman eklendi");
+            }
+        }
+
+       public void yazdir()
+        {
+            Node temp = head;
+
+            if( head == null)
+            {
+                Console.WriteLine("liste boş");
+            }
+            else
+            {
+                Console.Write(" baş ");
+                while( temp.next != null)
+                {
+                    Console.Write( temp.data + " -> ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + " son ");
+
+            }
+        }
+
+        public void bastanSil()
+        {
+            if ( head == null)
+            {
+                Console.WriteLine("liste boş");
+            }
+            else
+            {
+                head = head.next; // baştaki düğümü sildik
+                Console.WriteLine("baştan eleman silindi");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List tekYonListe = new List();
+
+            tekYonListe.sonaEkle(10);
+            tekYonListe.sonaEkle(20);
+            tekYonListe.sonaEkle(30);
+
+            tekYonListe.yazdir();
+
+            tekYonListe.basaEkle(5);
+            tekYonListe.sonaEkle(100);
+
+            tekYonListe.yazdir();
+
+            Console.WriteLine();
+
+            tekYonListe.bastanSil();
+            tekYonListe.bastanSil();
+
+            tekYonListe.yazdir();
+
+
+            Console.ReadKey();
+        }
+    }
+}
